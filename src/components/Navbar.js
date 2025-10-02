fa00b51490fc87a5fa8b481a7617d4deade314ef
Aiden Taylor <aidenjt@uci.edu>
2025-10-02 10:29:30 -0700

    Created basic layout + styling for the navbar.

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+import { ReactComponent as Logo } from '../AT-Logo.svg';
+
+function Navbar() {
+    return (
+        <nav>
+            <Link to="/">
+                <Logo className="logo" />
+            </Link>
+            <ul className="navbar-links">
+                <li><Link to="/About">About</Link></li>
+                <li><Link to="/Experience">Experience</Link></li>
+                <li><Link to="/Projects">Projects</Link></li>
+                <li><Link to="/Contact">Contact</Link></li>
+            </ul>
+        </nav>
+    );
+} 
+
+export default Navbar;
