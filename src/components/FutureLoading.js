8bbd2720fde5cc24b967cfce300a69575e6c252f
Aiden Taylor <aidenjt@uci.edu>
2025-10-03 17:23:56 -0700

    Created an animation where it says "loading..." indicating that my future is loading.

---
+++ src/components/FutureLoading.js
@@ -0,0 +1,14 @@
+import "../styles/FutureLoading.css";
+
+function FutureLoading() {
+    return (
+        <div className="future-window">
+            <div className="loading-text">
+                <span className="loading-word">Loading</span>
+                <span className="loading-dots"></span>
+            </div>
+        </div>
+    );
+}
+
+export default FutureLoading;
